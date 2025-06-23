a3497f6b3b5b8493de3716d9b5a2e70d76dce2f0
connerohnesorge <conneroisu@outlook.com>
2025-06-23 06:55:27 -0500

    latest

--- nordvpn.nix
+++ nordvpn.nix
@@ -14,9 +14,31 @@
   libidn2,
   zlib,
   wireguard-tools,
+  icu72,
 }: let
   pname = "nordvpn";
   version = "3.18.3";
+
+  # NordVPN requires an old libxml2 version (2.x with .so.2)
+  # We'll use Debian's package which is compatible
+  libxml2Legacy = stdenv.mkDerivation {
+    name = "libxml2-legacy";
+    src = fetchurl {
+      url = "http://ftp.debian.org/debian/pool/main/libx/libxml2/libxml2_2.9.14+dfsg-1.3~deb12u1_amd64.deb";
+      hash = "sha256-NbdstwOPwclAIEpPBfM/+3nQJzU85Gk5fZrc+Pmz4ac=";
+    };
+    
+    nativeBuildInputs = [ dpkg ];
+    
+    unpackPhase = ''
+      dpkg-deb -x $src .
+    '';
+    
+    installPhase = ''
+      mkdir -p $out
+      cp -r usr/lib $out/
+    '';
+  };
 
   nordVPNBase = stdenv.mkDerivation {
     inherit pname version;
@@ -26,7 +48,7 @@
       hash = "sha256-pCveN8cEwEXdvWj2FAatzg89fTLV9eYehEZfKq5JdaY=";
     };
 
-    buildInputs = [libxml2 libidn2];
+    buildInputs = [libidn2 icu72];
     nativeBuildInputs = [dpkg autoPatchelfHook stdenv.cc.cc.lib];
 
     dontConfigure = true;
@@ -46,11 +68,19 @@
       mv etc/ $out/
       runHook postInstall
     '';
+
+    # Skip autoPatchelfHook for nordvpnd since we'll patch it manually
+    autoPatchelfIgnoreMissingDeps = ["libxml2.so.2"];
+    
+    postFixup = ''
+      # Manually patch nordvpnd with the correct rpath
+      patchelf --set-rpath "${libxml2Legacy}/lib/x86_64-linux-gnu:${libidn2}/lib:${icu72}/lib:${stdenv.cc.cc.lib}/lib" $out/bin/nordvpnd
+    '';
   };
 
   nordVPNfhs = buildFHSEnvChroot {
     name = "nordvpnd";
-    runScript = "nordvpnd";
+    runScript = "${nordVPNBase}/bin/nordvpnd";
 
     targetPkgs = pkgs: [
       nordVPNBase
@@ -59,11 +89,17 @@
       iproute2
       procps
       cacert
-      libxml2
+      libxml2Legacy
       libidn2
       zlib
       wireguard-tools
+      icu72
     ];
+    
+    # Set up library paths for the FHS environment
+    profile = ''
+      export LD_LIBRARY_PATH="${libxml2Legacy}/lib/x86_64-linux-gnu:${icu72}/lib:$LD_LIBRARY_PATH"
+    '';
   };
 in
   stdenv.mkDerivation {
